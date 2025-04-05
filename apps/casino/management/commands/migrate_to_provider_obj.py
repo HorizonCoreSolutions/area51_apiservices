a2799de0d67202dfb351531958f1eb1b9376f409
MarcoAlv <101905177+MarcoAlv@users.noreply.github.com>
2025-04-04 20:07:26 -0500

    Update migrate provider

--- apps/casino/management/commands/migrate_to_provider_obj.py
+++ apps/casino/management/commands/migrate_to_provider_obj.py
@@ -20,7 +20,7 @@ class Command(BaseCommand):
 
             # Get provider_obj based on the vendor_name
             vendor_name = game.vendor_name
-            provider = self.fetch_provider_obj(vendor_name)
+            provider = self.fetch_provider_obj(vendor_name, game)
             
             # Store it
             game.provider = provider
@@ -29,10 +29,10 @@ class Command(BaseCommand):
             print(f"game {game.game_image} has been update to provider ({game.provider.name})")
 
 
-    def fetch_provider_obj(self, name: str) -> Providers:
-        queryset = Providers.objects.filter(name=name)
+    def fetch_provider_obj(self, name: str, game: CasinoGameList) -> Providers:
+        queryset = Providers.objects.filter(name=name, admin=game.casino_management.admin)
 
         if queryset.exists():
             return queryset.first()
 
-        return Providers.objects.create(name=name)
+        return Providers.objects.create(name=name, admin=game.casino_management.admin)
