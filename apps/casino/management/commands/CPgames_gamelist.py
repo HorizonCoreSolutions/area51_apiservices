f2a399dc2403469eb30a4693ef0791b56908e959
MarcoAlv <marcs2805@gmail.com>
2025-04-16 19:56:59 -0500

    modify gamelist

--- apps/casino/management/commands/CPgames_gamelist.py
+++ apps/casino/management/commands/CPgames_gamelist.py
@@ -16,7 +16,51 @@ class Command(BaseCommand):
 
     cp = CPgames()
     games = cp.get_games()
+    # name_en
+    # game_id
+    # type
 
 
     def handle(self, *args, **kwargs):
-        pass
+        if not self.games:
+            print("No games where given by the api")
+            return
+        print(f"TOTAL GAMES: {len(self.games)}")
+        for game in self.games:
+            if not game:
+                continue
+            obj, created = CasinoGameList.objects.update_or_create(
+                game_id=game.get("game_id"),
+                defaults={
+                    "game_name" : game.get("name_en"),
+                    "section_id" : "CPGames",
+                    "vendor_name" : "CPgames"
+                }
+            )
+
+            if created:
+                pass
+
+
+    def update_or_create_casino_management(self, game):
+        users = Users.objects.filter(role="admin")
+        for user in users:
+            obj, _ = CasinoManagement.objects.get_or_create(
+                admin = user,
+                game = game
+            )
+            obj.save()
+
+
+    def update_or_create_casino_categories(self):
+        categories = CasinoGameList.objects.distinct("game_category").values_list("game_category", flat=True)
+        for category in categories:
+            obj, created = CasinoHeaderCategory.objects.get_or_create(
+                name= category
+            )
+            if created:
+                cateogry = CasinoHeaderCategory.objects.filter(~Q(position=None)).order_by("position").last()
+                obj.position = cateogry.position + 1 if cateogry else 1
+                obj.save()
+
+
