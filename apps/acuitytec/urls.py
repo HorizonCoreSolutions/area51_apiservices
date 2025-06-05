bc3313925df9a6114336b1ac7f6654dc0d548be1
MarcoAlv <marcs2805@gmail.com>
2025-06-05 11:28:10 -0500

    add callback

--- apps/acuitytec/urls.py
+++ apps/acuitytec/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import url
 from django.urls import path
 
-from .views import GetVerificationLinkView
+from .views import GetVerificationLinkView, CallbackAcuitytecView
 
 app_name = "acuitytec"
 
 
 urlpatterns = [
-    path('url', GetVerificationLinkView.as_view(), name='get-link')
+    path('url', GetVerificationLinkView.as_view(), name='get-link'),
+    path('callback', CallbackAcuitytecView.as_view(), name='acuitytec-callback'),
 ]
