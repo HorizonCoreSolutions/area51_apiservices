9bed5c76b9203fc7ca7915bbad62083f90eb0dd7
MarcoAlv <marcs2805@gmail.com>
2025-06-05 10:11:23 -0500

    remove debbuging

--- apps/acuitytec/views.py
+++ apps/acuitytec/views.py
@@ -14,32 +14,28 @@ class GetVerificationLinkView(APIView):
         try:
             if not request.user.is_authenticated:
                 return Response({"message": "The user must be authenticated"}, status.HTTP_400_BAD_REQUEST)
-                
+
             user = Users.objects.get(id = request.user.id)
-            
+
             document = request.data.get('document')
             language = request.data.get('language')
-            print('ping')
-            
+
             if document is None or language is None:
-                print('bping')
                 return Response({"message": "language or document must not be None"}, status.HTTP_400_BAD_REQUEST)
-            
+
             if not language in ('es', 'en', 'fr'):
                 return Response({"message": "language must be one of the following us, es, fr"}, status.HTTP_400_BAD_REQUEST)
-                 
-            
+
             ac = AcuityTecAPI(user=user)
-            
+
             x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
-            
-            print('cping')
+
             if x_forwarded_for:
                 print(x_forwarded_for)
                 ip = x_forwarded_for.split(',')[0].strip()  # client’s real IP
             else:
                 ip = request.META.get('REMOTE_ADDR')
-            
+
             if not hasattr(user, 'acuitytec_account'):
                 res = ac.register_customer(ip)
                 print(res)
@@ -50,17 +46,19 @@ class GetVerificationLinkView(APIView):
                         )
                 if res['status'] == -1:
                     return Response({"message": res['message']}, status.HTTP_400_BAD_REQUEST)
-                    
-                    
-            print('dping')
-                
+
             link = ac.getLink(document=document, language=language)
-            print(link)
-            print('eping')
             if link.startswith('error'):
                 return Response({"message": link[5:]}, status.HTTP_400_BAD_REQUEST)
-                
+
             return Response({'url' : link}, status=status.HTTP_200_OK)
         except Exception as e:
             print(e)
             return Response({"message": "Something Went Wrong"}, status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+class CallbackAcuitytecView(APIView):
+
+    def post(self, request):
+        AcuityTecAPI.save_request(request=request)
+
