d91b06e862bd88d9c0ae74e136454f954fb41905
MarcoAlv <marcs2805@gmail.com>
2025-06-06 03:30:35 -0500

    add args_kargs

--- apps/acuitytec/management/commands/upgrade_multiple_verifications.py
+++ apps/acuitytec/management/commands/upgrade_multiple_verifications.py
@@ -5,7 +5,7 @@ from apps.users.models import Users
 class Command(BaseCommand):
     help = "Migrates the old users to the new countries"
 
-    def handle(self):
+    def handle(self, *args, **kwargs):
         counter = 0
         for user in Users.objects.all():
             user.phone_verified = 1 if user.is_verified else 0
