562a86368de20e2225b74f95dd415764b2476963
MarcoAlv <marcs2805@gmail.com>
2025-06-20 17:06:09 -0500

    fix ip

--- apps/acuitytec/acuitytec.py
+++ apps/acuitytec/acuitytec.py
@@ -397,7 +397,7 @@ class AcuityTecAPI:
         x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
 
         if x_forwarded_for:
-            ip = x_forwarded_for.split(',')[-1].strip()  # client’s real IP
+            ip = x_forwarded_for.split(',')[0].strip()  # client’s real IP
             data = x_forwarded_for
         else:
             ip = request.META.get('REMOTE_ADDR')
