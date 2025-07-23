c9ae5c21879c6e7184372a3bb241c660f8f1ea8f
MarcoAlv <marcs2805@gmail.com>
2025-07-23 00:19:34 -0500

    update payouts

---
+++ apps/payments/management/commands/coinflow_update_payout.py
@@ -0,0 +1,51 @@
+
+from datetime import datetime, timedelta
+from django.utils import timezone
+from django.conf import settings
+from django.core.management.base import BaseCommand
+import time
+
+import requests
+from apps.core.file_logger import SimpleLogger
+from apps.payments.models import CoinFlowTransaction
+from apps.payments.coinflow import CoinFlowEndpoints
+
+logger = SimpleLogger(name='Coinflow', log_file='logs/coinflow.log').get_logger()
+
+class Command(BaseCommand):
+    
+    def handle(self, *args, **kwargs):
+        try:
+            endpoint = CoinFlowEndpoints(settings.COINFLOW_API_URL)
+            while True:
+                one_week_ago = timezone.now() - timedelta(days=7)
+                transactions = CoinFlowTransaction.objects.filter(
+                    transaction_type = CoinFlowTransaction.TransactionType.withdraw,
+                    status = CoinFlowTransaction.StatusType.requested,)
+                for transaction in transactions:
+                    res = requests.get(endpoint.get_withdrawal.format(signature=transaction.signature))
+                    if res.status_code not in {200, 404}:
+                        logger.critical(f"Transaction get withdrawal give unespected code {res.status_code}, for signature:{transaction.signature}")
+                        break
+                    if res.status_code == 404:
+                        transaction.status = CoinFlowTransaction.StatusType.cancelled
+                    try:
+                        data = res.json()
+                        withdrawal = data.get("withdrawal", {})
+                        status = withdrawal.get("status")
+                        
+                        if status is None:
+                            logger.info(f"Withdrawal status is None on 200 for CoinFlowTransaction.transaction_id = {transaction.transaction_id}")
+                        
+                        if status == "completed":
+                            transaction.status =  CoinFlowTransaction.StatusType.paid_out
+                        if status == "failed":
+                            transaction.status =  CoinFlowTransaction.StatusType.failed
+                        transaction.save()
+                    except:
+                        logger.critical(f"Data for Signature couldn't be deserialized {transaction.signature}")
+                print(datetime.now())
+                print('Sleep for 4 hours')
+                time.sleep(60*60*4)      
+        except Exception as e:
+            print(e)
