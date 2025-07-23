a3c5fcafea6c13539ebf0ef270a4a16a7dc9820b
MarcoAlv <marcs2805@gmail.com>
2025-07-22 23:09:30 -0500

    fix cast

--- apps/core/utils.py
+++ apps/core/utils.py
@@ -17,9 +17,9 @@ def try_cast(value: Any, target_type: Optional[Type] = None) -> Union[Any, str]:
         return value
 
 def get_env_var(var_name: str, default: Any = None, cast: Optional[Type] = None) -> Any:
-    value = os.environ.get(var_name, default)
-
-    if value is None:
+    try:
+        value = os.environ.get(var_name, default)
+    except KeyError:
         error_msg = f"Set the {var_name} environment variable"
         logger.critical(error_msg)
         raise ImproperlyConfigured(error_msg)
