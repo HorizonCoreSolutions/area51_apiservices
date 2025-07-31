a22d87a7083de1ed3fdfd786309eb229efc265e7
MarcoAlv <marcs2805@gmail.com>
2025-07-30 20:18:24 -0500

    fix class issues

--- apps/acuitytec/utils.py
+++ apps/acuitytec/utils.py
@@ -1,4 +1,7 @@
+from typing import Dict, Union
 from urllib.parse import quote_plus
+
+from apps.acuitytec.models import IPLog
 
 def generate_qr_code_url(data: str, size: str = "150x150") -> str:
     """
@@ -13,3 +16,34 @@ def generate_qr_code_url(data: str, size: str = "150x150") -> str:
     """
     encoded_data = quote_plus(data)
     return f"https://api.qrserver.com/v1/create-qr-code/?size={size}&data={encoded_data}"
+
+
+
+def cache_ips(func):
+    """This is a wrapper to cache the is_geo_verified 
+    Args:
+        func (is_geo_verified): 
+    """
+    def wrapper(*args, **kwargs) -> Dict[str, Union[str, int]]:
+        ip = kwargs.get('ip')
+        if not ip:
+            raise ValueError("ip must be provided as a keyword argument: ip='1.1.1.1'")
+        
+        ip_obj = IPLog.objects.filter(ip=ip).first()
+        
+        if ip_obj is None:
+            res = func(*args, **kwargs)
+            status = res.get('status')
+            if status not in {0, -1}:
+                return res
+            ip_obj = IPLog.use_or_create(
+                ip=ip,
+                defaults={
+                    "status": status,
+                    "error_name" : res.pop("rule"),
+                    "display_message" : res.get('message')
+                }
+            )
+        return ip_obj.parse_geo()
+    
+    return wrapper
