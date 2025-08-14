13e227640d132d0fb0fa6bf06c8d2ae0248171a4
MarcoAlv <marcs2805@gmail.com>
2025-08-14 00:18:18 -0500

    stop

--- apps/casino/tasks.py
+++ apps/casino/tasks.py
@@ -10,6 +10,9 @@ from django.db import transaction as db_transaction
 # Retry delays in seconds
 RETRY_DELAYS = [15, 25, 70]
 
+class RetryableTransactionError(Exception):
+    """Raised when transaction should be retried."""
+
 @shared_task(bind=True, queue="casino_queue", max_retries=3)
 def task_update_offmarket_transaction(self, transaction_id):
     transaction = OffMarketTransactions.objects.filter(
@@ -43,7 +46,7 @@ def task_update_offmarket_transaction(self, transaction_id):
         with db_transaction.atomic():
             if response.status_code != status.HTTP_200_OK:
                 print(f"Transaction {transaction_id}: received non-200 status {response.status_code}. Retrying.")
-                _retry_with_delay(self, Exception(f"Non-200: {response.status_code}"))
+                raise RetryableTransactionError(f"Non-200: {response.status_code}")
 
             if trx_status == 'Completed':
                 transaction.status = 'Completed'
@@ -55,21 +58,22 @@ def task_update_offmarket_transaction(self, transaction_id):
                 transaction.status = 'Failed'
             else:
                 print(f"Unexpected status {trx_status} for transaction {transaction_id}")
-                _retry_with_delay(self, Exception(f"Unexpected status: {trx_status}"))
+                raise RetryableTransactionError(f"Unexpected status: {trx_status}")
             transaction.save()
 
-    except requests.RequestException as e:
+    except (requests.RequestException, RetryableTransactionError) as e:
         print(f"Error updating transaction {transaction_id}: {e}")
         _retry_with_delay(self, e)
     except Exception as e:
-        print(f"Error updating transaction {transaction_id}: {e}")
+        print(f"Unspected error updating transaction {transaction_id}: {e}")
         _retry_with_delay(self, e)
 
 
 def _retry_with_delay(task_self, exc):
     retry_count = task_self.request.retries  # 0 for first retry, 1 for second, etc.
     if retry_count >= 3:  # already retried 3 times
-        return
+        print(f"[Fail] Task {task_self.request.id} reached max retries ({task_self.max_retries}) and failed permanently.")
+        raise exc
     delay = RETRY_DELAYS[retry_count] if retry_count < len(RETRY_DELAYS) else RETRY_DELAYS[-1]
     print(f"[Retry] Task {task_self.request.id}: attempt {retry_count + 1} failed. Retrying in {delay} seconds.")
-    raise task_self.retry(exc=exc, countdown=delay)
+    raise task_self.retry(exc=exc, countdown=delay, throw=False)
