fe379900baedbe6431b239f536158e3d397b3c75
MarcoAlv <marcs2805@gmail.com>
2025-08-27 03:08:21 -0500

    Update Game List CPgames

--- apps/casino/management/commands/CPgames_gamelist.py
+++ apps/casino/management/commands/CPgames_gamelist.py
@@ -1,16 +1,15 @@
-import json
-import time
-import requests
-
 from django.db.models import Q
 from datetime import timedelta
-from django.conf import settings
 from django.utils import timezone
 from django.core.management.base import BaseCommand
 
 from apps.casino.cpgames import CPgames
 from apps.users.models import Users
-from apps.casino.models import CasinoGameList, CasinoHeaderCategory, CasinoManagement, Providers
+from apps.casino.models import (
+        CasinoGameList,
+        CasinoHeaderCategory,
+        CasinoManagement
+        )
 
 
 class Command(BaseCommand):
@@ -22,8 +21,8 @@ class Command(BaseCommand):
     # game_id
     # type
     change_to_stable = {
-        "SLOTS" : "Slots",
-        "Mini Games" : "Mini Games",
+        "SLOTS": "Slots",
+        "Mini Games": "Mini Games",
     }
     DEMO_GAMES = [
         "1_16",
@@ -45,7 +44,6 @@ class Command(BaseCommand):
         "2_1700071"
     ]
 
-
     def handle(self, *args, **kwargs):
         if not self.games:
             print("No games where given by the api")
@@ -55,7 +53,9 @@ class Command(BaseCommand):
         for game in self.games:
             if not game:
                 continue
-            game_id = game.get("game_id")
+            game_id = game.get("game_id", "lobby")
+            if game_id.lower() == "lobby":
+                continue
             casino_game_ids.append(game_id)
             game_cat = self.change_to_stable.get(game.get("type", "SLOTS"), "Slots")
             print(f"Game saved: {game.get('name_en')}\nType: {game_cat}\nID: {game_id}")
@@ -63,13 +63,13 @@ class Command(BaseCommand):
             obj, created = CasinoGameList.objects.update_or_create(
                 game_id=game_id,
                 defaults={
-                    "game_name" : game.get("name_en"),
-                    "section_id" : "CPgames",
-                    "vendor_name" : "CPgames",
-                    "game_category" : game_cat,
-                    "is_mobile_supported" : True,
-                    "is_desktop_supported" : True,
-                    "is_free_round_supported" : game_id in self.DEMO_GAMES
+                    "game_name": game.get("name_en"),
+                    "section_id": "CPgames",
+                    "vendor_name": "CPgames",
+                    "game_category": game_cat,
+                    "is_mobile_supported": True,
+                    "is_desktop_supported": True,
+                    "is_free_round_supported": game_id in self.DEMO_GAMES
                 }
             )
             if created:
@@ -78,27 +78,27 @@ class Command(BaseCommand):
             obj.save()
             self.update_or_create_casino_categories()
 
-        CasinoGameList.objects.filter( Q(vendor_name="CPgames") & ~Q(game_id__in=casino_game_ids)).delete()
+        CasinoGameList.objects.filter(
+                Q(vendor_name="CPgames") & ~Q(game_id__in=casino_game_ids)
+                ).delete()
 
     def update_or_create_casino_management(self, game):
         users = Users.objects.filter(role="admin")
         for user in users:
             obj, _ = CasinoManagement.objects.get_or_create(
-                admin = user,
-                game = game
+                admin=user,
+                game=game
             )
             obj.save()
 
-
     def update_or_create_casino_categories(self):
-        categories = CasinoGameList.objects.distinct("game_category").values_list("game_category", flat=True)
+        categories = CasinoGameList.objects.distinct(
+                "game_category").values_list("game_category", flat=True)
         for category in categories:
             obj, created = CasinoHeaderCategory.objects.get_or_create(
-                name= category
+                name=category
             )
             if created:
                 cateogry = CasinoHeaderCategory.objects.filter(~Q(position=None)).order_by("position").last()
                 obj.position = cateogry.position + 1 if cateogry else 1
                 obj.save()
-
-
