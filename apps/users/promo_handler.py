4962f50801ee892fc4139c507e4c18fdf8fb736c
MarcoAlv <marcs2805@gmail.com>
2025-09-09 15:58:36 -0500

    create promo handler

---
+++ apps/users/promo_handler.py
@@ -0,0 +1,16 @@
+def create_code():
+    pass
+
+def redeam_code():
+    # Filter object by created date and
+    # end date and verification also code
+    # and type
+
+    # 
+    pass
+
+def verify_code() -> bool:
+    return False
+
+def partial_redeam():
+    pass
