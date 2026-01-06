d919bf3b62514b383a40ee54a552c166e7915f1c
MarcoAlv <marcs2805@gmail.com>
2026-01-06 13:02:27 -0500

    Fix wageting

--- apps/bets/repository.py
+++ apps/bets/repository.py
@@ -1,29 +1,32 @@
-from decimal import Decimal
 from typing import Dict
-
+from decimal import Decimal
+from django.conf import settings
+from apps.users.models import Users
 from django.db.models import Q, Count, Sum
 from apps.bets.models import WageringRequirement
-from apps.users.models import Users
 
 
 def get_react_bonus_amount(user: Users) -> Dict:
-    data = WageringRequirement.objects.filter(
+    base = WageringRequirement.objects.filter(
         user=user,
         active=True,
         betable=False,
-    ).aggregate(
+    )
+    
+    data = base.aggregate(
         amount=Count('id'),
         pool_amount=Sum('balance'),
         generated=Sum("result", filter=Q(result__isnull=False)),
     )
-    oldest = WageringRequirement.objects.filter(
-        user=user,
-        active=True,
-        betable=False,
-    ).order_by('created').values('played').first()
-    percentage = (((oldest.get('played') or 0) % 20) * 5) // 1 if oldest else Decimal('0.00')
+    oldest = base.order_by('created').first()
+    if oldest:
+        cycle_progress = (oldest.limit / oldest.amount) * 100
+        percentage = (((oldest.played % oldest.limit) * (100 / cycle_progress)) // 1)
+    else:
+        cycle_progress = settings.REACTOR_MULTIPLIER
+        percentage = Decimal('0.00')
     return {
-        'cycle_progress': 20,
+        'cycle_progress': cycle_progress,
         'cycle_progress_ratio': 1,
         'next_reward': 1,
         'pool_amount': data.get('pool_amount') or 0,
