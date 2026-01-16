0139dacf42c9464bfd0b66d9b42e34c6ce403252
MarcoAlv <marcs2805@gmail.com>
2026-01-16 17:39:59 -0500

    new fields

--- apps/users/models.py
+++ apps/users/models.py
@@ -416,6 +416,13 @@ class Users(AbstractBaseUser, AbstractBaseModel, PermissionsMixin):
         _("payout balance"), max_digits=15, decimal_places=2, default=0.00, null=False, blank=False
     )
 
+    balance_reactor = models.DecimalField(
+        _("balance reactor"), max_digits=15, decimal_places=2, default=0.00, null=False, blank=False
+    )
+    balance_wagering = models.DecimalField(
+        _("balance wagering"), max_digits=15, decimal_places=2, default=0.00, null=False, blank=False
+    )
+
     weekly_dl = models.DecimalField(
         max_digits=15, decimal_places=2, default=None, null=True, blank=False
     )
