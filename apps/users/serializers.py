50c4b394cabc012b255a73eb16a97fa0c7fb9448
MarcoAlv <marcs2805@gmail.com>
2026-01-19 19:20:14 -0500

    Fix simgle loggin

--- apps/users/serializers.py
+++ apps/users/serializers.py
@@ -379,7 +379,7 @@ class LoginSerializer(JSONWebTokenSerializer):
                 )
             user = authenticate(**credentials)
             if (user and
-                settings.MULTI_DEVICE_LOGIN_ENABLED and
+                not settings.MULTI_DEVICE_LOGIN_ENABLED and
                 user.is_currently_active and
                 user.last_activity_time > timezone.now()-timedelta(minutes=15)
             ):  
